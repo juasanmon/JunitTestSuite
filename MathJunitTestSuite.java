6b7e1e8256a4f66c319385e7f78bd8111c4269fe
juasanmon <manowar1995@gmail.com>
2017-05-17 09:22:35 +0200

    Add files via upload

---
+++ MathJunitTestSuite.java
@@ -0,0 +1,12 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   MathTest.class,
+   TestPerson.class
+})
+public class MathJunitTestSuite {   
+	
+
+}  	
